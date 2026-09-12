1bca2ec025d0620ebcb21ac98b11e921d23efdc0
Nimantha Ravindu <nimantharavindu713@gmail.com>
2026-09-12 10:06:21 +0530

    fix: make account number required to prevent P2011 error

--- app/dashboard/branches/[id]/accounts/page.tsx
+++ app/dashboard/branches/[id]/accounts/page.tsx
@@ -102,7 +102,6 @@ revalidatePath(`/dashboard/branches/${branchId}/accounts`);
             <option value="Router Bill">Router Bill</option>
             <option value="Courier Bill">Courier Bill</option>
             <option value="Vehicle Bill">Vehicle Bill</option>
-            <option value="Petty Cash">Petty Cash</option>
             <option value="IOU Bill">IOU Bill</option>
             <option value="Other">Other</option>
           </select>
